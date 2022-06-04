700e370d2b65013dadce301f0d1e50b08d7cb783
Isaque Terra <terraisaque@gmail.com>
2022-06-04 16:10:38 -0300

    card 2 - Creating the first page

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import Index from './pages/home';
+import './pages/home/index.scss'
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    
+     <Index />
   </React.StrictMode>
 );
 
